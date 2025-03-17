9ac767b4442b20514d60fb121946b315ad9e7083
Tungsteno74 <tungsteno74@yahoo.it>
2025-03-17 01:36:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,32 @@
 import os
+from setuptools import setup, find_packages
 
-from setuptools import find_namespace_packages, setup
+here = os.path.abspath(os.path.dirname(__file__))
 
-from __init__ import __version__
+with open(os.path.join(here, "kivy_garden", "simpletablelayout", "readme.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
-def read(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
-        return f.read()
-
-
-# exposing the params so it can be imported
-setup_params = {
-    'name': 'kivy_garden.simpletablelayout',
-    'version': __version__,
-    'description': 'Implements table layout with rowspan / colspan capabilities (like HTML tables)',
-    'long_description': read('README.md'),
-    'long_description_content_type': 'text/markdown',
-    'author': 'Kivy',
-    'author_email': 'kivy@kivy.org',
-    'keywords': 'Kivy kivy-garden',
-    'url': 'https://github.com/kivy-garden/garden.simpletablelayout',
-    'packages': find_namespace_packages(),
-    'package_data': {
-        'kivy_garden.simpletablelayout': ['*.kv'],
-    },
-    'python_requires': '>=3',
-    'install_requires': [
-        'kivy',
+setup(
+    name="kivy-garden.simpletablelayout", 
+    version="0.1",
+    author="Kivy",
+    author_email="kivy@kivy.org",
+    description="Simple Table Layout – Layout simile a una tabella HTML che supporta rowspan e colspan, integrato in Kivy Garden.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tungsteno74/garden.simpletablelayout", 
+    packages=find_packages(),
+    namespace_packages=["kivy_garden"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: User Interfaces",
     ],
-}
-
-
-def run_setup():
-    setup(**setup_params)
-
-
-# makes sure the setup doesn't run at import time
-if __name__ == '__main__':
-    run_setup()
+    python_requires=">=3.0",
+    install_requires=["kivy>=2.0.0"],
+)
